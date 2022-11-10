f7d2bca62a41d0cacb4b2d675997c27e3ef82428
ruleBased848 <rkdfowns@naver.com>
2022-11-10 19:49:54 +0900

    feat: add app bar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Gamsi Bot</Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
