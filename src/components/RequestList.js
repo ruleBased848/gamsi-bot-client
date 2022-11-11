e24b9c8740e44b1fe805fbd8c9f82d6b0fc47a9b
ruleBased848 <rkdfowns@naver.com>
2022-11-12 00:10:14 +0900

    feat: add AddRequest component which allows to send request to server

--- src/components/RequestList.js
+++ src/components/RequestList.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import AddRequest from './AddRequest';
 
 function RequestList() {
   const [requests, setRequests] = useState([]);
@@ -17,6 +18,29 @@ function RequestList() {
       .catch(err => console.error(err));
   };
 
+  const sendRequest = async request => {
+    if (request.channelId.trim().length === 0) return alert('유튜브 채널 ID는 필수 항목입니다');
+    if (request.emailAddress.length === 0) return alert('이메일 주소는 필수 항목입니다');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(request.emailAddress)) return alert('이메일 주소의 형식이 올바르지 않습니다');
+    const response = await fetch(process.env.REACT_APP_REQUEST_ENDPOINT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(request),
+    });
+    const json = await response.json();
+    let message = '';
+    if (response.ok) {
+      message = '요청이 정상적으로 수락되었습니다';
+      setRequests([json, ...requests]);
+    } else {
+      for (let [key, value] of Object.entries(json)) {
+        message += `${key}: ${value}\n`;
+      }
+    }
+    alert(message);
+  };
+
   const columns = [
     { field: 'channelId', headerName: '유튜브 채널 ID', width: 200 },
     { field: 'targetSubscriberCount', headerName: '목표 구독자 수', width: 200 },
@@ -32,6 +56,7 @@ function RequestList() {
 
   return (
     <>
+      <AddRequest sendRequest={sendRequest} />
       <div style={{ height: 500, width: '100%' }}>
         <DataGrid
           rows={requests}
