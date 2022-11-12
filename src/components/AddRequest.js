d1db9364180cef8498b628170e656408fb1e8a76
ruleBased848 <rkdfowns@naver.com>
2022-11-12 20:47:44 +0900

    feat: use mui components where possible

--- src/components/AddRequest.js
+++ src/components/AddRequest.js
@@ -3,6 +3,9 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
 function AddRequest({ sendRequest }) {
   const [open, setOpen] = useState(false);
@@ -22,31 +25,39 @@ function AddRequest({ sendRequest }) {
 
   return (
     <div>
-      <button onClick={handleClickOpen}>요청 추가</button>
+      <Button variant="contained" onClick={handleClickOpen}>요청 추가</Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>새 요청</DialogTitle>
         <DialogContent>
-          <label htmlFor="channelId">유튜브 채널 ID</label><br />
-          <input placeholder="UC?????" name="channelId" value={request.channelId} onChange={handleChange} /><br />
-          <label htmlFor="targetSubscriberCount">목표 구독자 수</label><br />
-          <input
-            type="number"
-            name="targetSubscriberCount"
-            value={request.targetSubscriberCount}
-            onChange={handleChange}
-          /><br />
-          <label htmlFor="emailAddress">이메일 주소</label><br />
-          <input
-            type="email"
-            placeholder="example@example.com"
-            name="emailAddress"
-            value={request.emailAddress}
-            onChange={handleChange}
-          /><br />
+          <Stack spacing={2} mt={1}>
+            <TextField
+              label="유튜브 채널 ID"
+              name="channelId"
+              autoFocus
+              variant="standard"
+              value={request.channelId}
+              onChange={handleChange}
+            />
+            <TextField
+              label="목표 구독자 수"
+              type="number"
+              name="targetSubscriberCount"
+              variant="standard"
+              value={request.targetSubscriberCount}
+              onChange={handleChange}
+            />
+            <TextField
+              label="이메일 주소"
+              name="emailAddress"
+              variant="standard"
+              value={request.emailAddress}
+              onChange={handleChange}
+            />
+          </Stack>
         </DialogContent>
         <DialogActions>
-          <button onClick={handleClose}>닫기</button>
-          <button onClick={handleAdd}>추가</button>
+          <Button onClick={handleClose}>닫기</Button>
+          <Button onClick={handleAdd}>추가</Button>
         </DialogActions>
       </Dialog>
     </div>
